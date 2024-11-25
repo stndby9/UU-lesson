bfa3e23ef881776c7ee1ddc7e1acde42ade94aa3
stndby9 <stndby9@gmail.com>
2024-11-25 15:40:42 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+#Практическое задание по теме "Переменные"
+count_homeworks=12
+count_hours=1.5
+name_course='Python'
+lead_time=count_hours/count_homeworks
+com=',' # запятая
+print('Курс:',name_course,com, 'всего задач:',count_homeworks,com, 'затрачено часов:',count_hours,com, 'среднее время выполнения',lead_time,'часа.')
