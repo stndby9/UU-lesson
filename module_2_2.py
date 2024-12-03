b7712999aef1b66db33f569591c99f6547c08c2c
stndby9 <stndby9@gmail.com>
2024-12-03 14:43:55 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = (input ('введите первое число: ' ))
+second = (input ('введите второе число: ' ))
+third = (input ('введите третье число: ' ))
+if first == second and first == third and first == third:
+    print ('3')
+elif first != second and second != third and first != third:
+    print ('0')
+else: print('2')
+
