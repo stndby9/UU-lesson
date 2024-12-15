468ce1bb089e87be5dfc044dab7af3e9c93c0943
stndby9 <stndby9@gmail.com>
2024-12-15 11:07:58 +0700

    Add files via upload

---
+++ recursion_.py
@@ -0,0 +1,15 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = (int(str_number[0]))
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    if first != 0:
+        return first
+    else:
+        return 1
+
+
+result = get_multiplied_digits(40203)
+print(result)
+result2 = get_multiplied_digits(4020300)
+print(result2)
